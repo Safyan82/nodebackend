b9290ac026842743fcbec43d11673533b05bbb0d
Safyan82 <33317298+Safyan82@users.noreply.github.com>
2021-10-20 14:13:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -56,8 +56,6 @@ const corsOptionsDelegate = (req, callback) => {
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Authorization, x-auth-token, content-type');
-    res.setHeader('Access-Control-Allow-Credentials', false);
     next();
   }); 
 
